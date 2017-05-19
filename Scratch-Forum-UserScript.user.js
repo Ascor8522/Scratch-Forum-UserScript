aa5f623e5bcaf9d419d40ce304c64c05e277659a
Smrman <vinoa.89.jeux@gmail.com>
2017-05-19 19:09:21 +0200

    Add files via upload

---
+++ Scratch-Forum-UserScript.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Scratch-Forum-UserScript
+// @namespace    http://github.com/Smrman/Scratch-Forum-UserScript
+// @version      1.0
+// @author       Smrman
+// @description  Adds buttons in html source code on the forum pages of Scratch.
+// @homepageURL  http://github.com/Smrman/Scratch-Forum-UserScript
+// @iconURL      https://raw.githubusercontent.com/Smrman/Scratch-Forum-UserScript/master/script-icon128x128.png
+// @icon64URL    https://raw.githubusercontent.com/Smrman/Scratch-Forum-UserScript/master/script-icon64x64.png
+// @updateURL
+// @downloadURL
+// @supportURL   !!!!!!!!!!!!!!
+// @include      https://scratch.mit.edu/discuss/topic/*
+// @match        https://scratch.mit.edu/discuss/topic/*
+// @exclude
+// @require      https://raw.githubusercontent.com/Smrman/Scratch-Forum-UserScript/master/Scratch-Forum-UserScript.js
+// @resource
+// @connect
+// @run-at       document-idle
+// @grant        none
+// @unwrap
+// @nocompat Chrome
+// ==/UserScript==
